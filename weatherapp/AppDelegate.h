de9f90505c55a5fe6ecf7169b27852331fb06d09
Kyle <kylechen004@gmail.com>
2017-03-31 07:27:44 -0700

    Initial Commit

---
+++ weatherapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weatherapp
+//
+//  Created by Kyle Chen on 3/31/17.
+//  Copyright © 2017 Kyle Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weatherapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weatherapp
+//
+//  Created by Kyle Chen on 3/31/17.
+//  Copyright © 2017 Kyle Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
